c4becc81e18aa131a97d9265b273f08c258c7dcb
MrSeager <seager1993@gmail.com>
2024-04-29 11:00:26 +0300

    4/29/2024

--- src/index.js
+++ src/index.js
@@ -1,37 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Container, Card, Image, Button } from 'react-bootstrap';
+import autoAnimate from '@formkit/auto-animate';
+import { Container, Card, Nav, Button } from 'react-bootstrap';
+import { FaLinkedin, FaGithub, FaDeviantart, FaYoutube } from 'react-icons/fa';
 import axios from 'axios';
 
 const images = require.context('./images', false, /\.(png|jpe?g|svg)$/);
 const imageList = images.keys().map(image => images(image));
 
 const MainPage = () => {
-  const [portfolios, setPortfolios] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const parentRef = useRef(null);
 
   useEffect(() => {
     axios.get('https://raw.githubusercontent.com/MrSeager/gallery-page/main/src/data.json').then((response) => {
-      setPortfolios(response.data.portfolio);
+      setPortfolio(response.data.portfolio);
 
     });
   }, []);
 
+  useEffect(() => {
+    if (parentRef.current) {
+      autoAnimate(parentRef.current);   
+    }
+  }, [parentRef]);
+
   return(
-    <Container className='d-flex flex-wrap'>
-      {portfolios.map((portfolio, index) => (
-        <Card className='flex-item m-4'>
-          <Card.Img fluid src={imageList[index]} alt={`Image ${index}`} />
-          <Card.ImgOverlay>
-            <Card.Title className='text-white'>{portfolio.title}</Card.Title>
-            <Card.Text className='text-white'>{portfolio.discription}</Card.Text>
-            <Button src={portfolio.link}>Link</Button>
-          </Card.ImgOverlay>
-        </Card>
-      ))}
-    </Container>
+    <main className='bg-light'>
+      <Nav className='bg-dark mb-5 text-light'>
+        <h1 className='display-2 text-center m-5 w-100'>Mr. Seager's Portfolio</h1>
+      </Nav>
+      <Container className='cs-grid' ref={parentRef}>
+        {portfolio.map((portfolio, index) => (
+          <Card className='m-3 bg-dark'>
+            <Card.Img fluid src={imageList[index]} alt={`Image ${index}`} />
+            <Card.Body className='d-flex flex-column justify-content-between'>
+              <Card.Title className='text-light text-center'>{portfolio.title}</Card.Title>
+              <Card.Text className='text-light'>{portfolio.discription}</Card.Text>
+              <Button variant="outline-light" className='' href={portfolio.link} target='_blank' rel="noopener noreferrer">Link</Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </Container>
+      <Container fluid className='mt-5 bg-dark p-4 d-flex flex-column flex-sm-row justify-content-end'>
+        <Button variant="outline-light" className="m-2" href='https://www.linkedin.com/in/sergiy-but-623426159/' target='_blank'>
+          <FaLinkedin size="1.5em" /> Linkedin
+        </Button>
+        <Button variant="outline-light" className="m-2" href='https://github.com/MrSeager' target='_blank'>
+          <FaGithub size="1.5em" /> Github
+        </Button>
+        <Button variant="outline-light" className="m-2" href='https://www.deviantart.com/mrseager29' target='_blank'>
+          <FaDeviantart size="1.5em" /> Deviantart
+        </Button>
+        <Button variant="outline-light" className="m-2" href='https://www.youtube.com/channel/UCQgY4AFsqQWgxOvdeNGs-cQ' target='_blank'>
+          <FaYoutube size="1.5em" /> YouTube
+        </Button>
+      </Container>
+    </main>
   );
 };
 
