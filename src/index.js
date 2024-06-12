35ae040af3961d8b702e3e01cbeb8fa9414969c3
MrSeager <seager1993@gmail.com>
2024-06-12 10:56:29 +0300

    6/12/2024

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import autoAnimate from '@formkit/auto-animate';
-import { Container, Card, Nav, Button, Row, Col, Badge, ButtonGroup, Form } from 'react-bootstrap';
+import { Container, Card, Nav, Button, Row, Col, Badge, ButtonGroup, Form, Dropdown } from 'react-bootstrap';
 import { FaLinkedin, FaGithub, FaDeviantart, FaYoutube, FaExternalLinkAlt, FaReact, FaBootstrap, FaGripLines } from 'react-icons/fa';
 import { PiFileHtmlDuotone, PiFileCssDuotone, PiDesktopTower, PiDeviceMobileThin } from "react-icons/pi";
 import { RiJavascriptFill } from "react-icons/ri";
@@ -16,12 +16,12 @@ import 'aos/dist/aos.css';
 
 const MainPage = () => {
   const [portfolio, setPortfolio] = useState([]);
-  const [currTheme, setCurrTheme] = useState(1);
+  const [currTheme, setCurrTheme] = useState(localStorage.getItem('currTheme') !== 0 && 1);
   const [showBtnGroup, setShowBtnGroup] = useState(false);
   const [selectedTechnology, setSelectedTechnology] = useState('');
-  const parentRef = useRef(null);
+  const [parent, enableAnimations] = useAutoAnimate();
 
-  const [animationParent] = useAutoAnimate();
+  const [stopAnim, setStopAnim] = useState('fade-down');
 
   useEffect(() => {
     axios.get('https://raw.githubusercontent.com/MrSeager/gallery-page/main/src/data.json').then((response) => {
@@ -29,42 +29,60 @@ const MainPage = () => {
     });
   }, []);
 
-  useEffect(() => {
-    if (parentRef.current) {
-      autoAnimate(parentRef.current);   
-    }
-  }, [parentRef]);
-
   const handleBtnGroup = () => {
     setShowBtnGroup(!showBtnGroup);
   };
 
-  AOS.init();
+  useEffect(() => {
+    // Initialize AOS when the component mounts
+    AOS.init();
+
+    // Clean up AOS after the initial animation
+    return () => {
+      AOS.refreshHard();
+    };
+  }, []);
+
+  // Set the current theme in localStorage when it changes
+  const handleThemeChange = (themeNumber) => {
+    setCurrTheme(themeNumber);
+    localStorage.setItem('currTheme', themeNumber);
+  };
+
+  // Retrieve the theme from localStorage when the component mounts
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('currTheme');
+    if (savedTheme) {
+      setCurrTheme(Number(savedTheme));
+    }
+  }, []);
 
   return(
-    <main className='bg-white' ref={animationParent}>
-      <Nav className={`cs-bg-el-${currTheme} shadow-sm m-0 text-white`} >
+    <main className='bg-white' ref={parent}>
+      <Nav data-aos={stopAnim} className={`cs-bg-el-${currTheme} shadow-sm m-0 text-white`} >
         <h1 className='display-2 text-center m-5 w-100 pe-none'>Mr. Seager's Portfolio</h1> 
         <Button variant='custom' onClick={handleBtnGroup} className='text-white mx-auto'><FaGripLines /></Button>
       </Nav>
       {showBtnGroup && (
         <ButtonGroup className='p-0 w-100 m-0 cs-h'>
-          <Button onClick={() => setCurrTheme(1)} className='cs-bg-el-1 border-0 cs-btngroup'></Button>
-          <Button onClick={() => setCurrTheme(2)} className='cs-bg-el-2 border-0'></Button>
-          <Button onClick={() => setCurrTheme(3)} className='cs-bg-el-3 border-0'></Button>
-          <Button onClick={() => setCurrTheme(4)} className='cs-bg-el-4 border-0 cs-btngroup'></Button>
+          <Button onClick={() => {setCurrTheme(1); setStopAnim(''); handleThemeChange(1)}} className='cs-bg-el-1 border-0 cs-btngroup'></Button>
+          <Button onClick={() => {setCurrTheme(2); setStopAnim(''); handleThemeChange(2)}} className='cs-bg-el-2 border-0'></Button>
+          <Button onClick={() => {setCurrTheme(3); setStopAnim(''); handleThemeChange(3)}} className='cs-bg-el-3 border-0'></Button>
+          <Button onClick={() => {setCurrTheme(4); setStopAnim(''); handleThemeChange(4)}} className='cs-bg-el-4 border-0 cs-btngroup'></Button>
         </ButtonGroup>
         )}
-      <Form.Select onChange={(e) => setSelectedTechnology(e.target.value)} value={selectedTechnology} 
-                   className={`cs-select border-0 cs-w-2 my-4 mx-auto mx-lg-5 ms-lg-auto cs-bg-el-${currTheme} text-white text-center px-0`}>
-        <option value=''>Technology</option>
-        <option value='html'>HTML</option>
-        <option value='css'>CSS</option>
-        <option value='javascript'>JavaScript</option>
-        <option value='react'>React</option>
-        <option value='bootstrap'>Bootstrap</option>
-      </Form.Select>
-      <Container className='cs-grid mt-3' ref={animationParent}>
+      <Dropdown className='cs-w-2 mx-auto ms-lg-auto me-lg-5'>
+        <Dropdown.Toggle className={`text-uppercase my-3 cs-select border-0 w-100 cs-bg-el-${currTheme} text-white text-center px-0`}>{selectedTechnology !== '' ? selectedTechnology : 'technology'}</Dropdown.Toggle>
+        <Dropdown.Menu className={`w-100 text-center text-uppercase cs-bg-el-${currTheme}`}>
+          <Dropdown.Item onClick={(e) => setSelectedTechnology('')} className={`border-0 cs-btn-${currTheme}`}>Technology</Dropdown.Item>
+          <Dropdown.Item onClick={(e) => setSelectedTechnology('html')} className={`border-0 cs-btn-${currTheme}`}>HTML</Dropdown.Item>
+          <Dropdown.Item onClick={(e) => setSelectedTechnology('css')} className={`border-0 cs-btn-${currTheme}`}>CSS</Dropdown.Item>
+          <Dropdown.Item onClick={(e) => setSelectedTechnology('javascript')} className={`border-0 cs-btn-${currTheme}`}>JavaScript</Dropdown.Item>
+          <Dropdown.Item onClick={(e) => setSelectedTechnology('react')} className={`border-0 cs-btn-${currTheme}`}>React</Dropdown.Item>
+          <Dropdown.Item onClick={(e) => setSelectedTechnology('bootstrap')} className={`border-0 cs-btn-${currTheme}`}>Bootstrap</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Container className='cs-grid mt-3'>
         {portfolio
           .filter((portfolio) => selectedTechnology === '' || portfolio.technology.includes(selectedTechnology))
           .map((portfolio, index) => (
